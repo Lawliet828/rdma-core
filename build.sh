8bd1f16c94499aa21d6b8a281f3c8e509775c5fa
jie.jiang <jiecfd@gmail.com>
2025-02-21 15:32:53 +0800

    build.sh support install

--- build.sh
+++ build.sh
@@ -23,9 +23,9 @@ fi
 cd "$BUILDDIR"
 
 if [ "x$NINJA" == "x" ]; then
-    $CMAKE -DIN_PLACE=1 ${EXTRA_CMAKE_FLAGS:-} ..
+    $CMAKE -DCMAKE_INSTALL_PREFIX=/usr/local ${EXTRA_CMAKE_FLAGS:-} ..
     make
 else
-    $CMAKE -DIN_PLACE=1 -GNinja ${EXTRA_CMAKE_FLAGS:-} ..
+    $CMAKE -DCMAKE_INSTALL_PREFIX=/usr/local -GNinja ${EXTRA_CMAKE_FLAGS:-} ..
     $NINJA
 fi
